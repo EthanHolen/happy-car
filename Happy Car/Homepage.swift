e5e665bdba37f0077ecc12a1fec3e82209dca379
EthanHolen <ethanholen@gmail.com>
2021-03-13 19:16:24 -0700

    create list of cars and filler data

--- Happy Car/Homepage.swift
+++ Happy Car/Homepage.swift
@@ -8,14 +8,46 @@
 import SwiftUI
 
 struct Homepage: View {
+    @Environment(\.managedObjectContext) var moc
+    @FetchRequest(entity: Vehicle.entity(), sortDescriptors: []) var vehicles:
+        FetchedResults<Vehicle>
+    
+    
     var body: some View {
         NavigationView {
             VStack{
-                
-                Text("Welcome to your happy car!")
-                
+                List {
+                    ForEach(vehicles, id:\.self){ vehicle in
+                        NavigationLink(destination: Text(vehicle.wrappedName)) {
+                            Text(vehicle.wrappedName)
+                        }
+                    }
+                }
+                Button("ADD"){
+                    
+                    let vehicle1  = Vehicle(context: self.moc)
+                    vehicle1.name = "Maggie"
+                    vehicle1.make = "Honda"
+                    vehicle1.model = "Accord"
+                    
+                    let vehicle2  = Vehicle(context: self.moc)
+                    vehicle2.name = "Oliver"
+                    vehicle2.make = "Nissan"
+                    vehicle2.model = "Altima"
+                    
+                    let vehicle3  = Vehicle(context: self.moc)
+                    vehicle3.name = "Quincy"
+                    vehicle3.make = "Nissan"
+                    vehicle3.model = "Murano"
+                    
+                    if self.moc.hasChanges  {
+                        try? self.moc.save()
+                    }
+                        
+                }
             }
-            .navigationBarTitle(Text("Happy Car"))
+            .navigationBarTitle("Happy Car")
+            
             
         }
     }
