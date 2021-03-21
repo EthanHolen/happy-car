8dbd64a72e1dc752490b968ab5b17a254f0f0b6e
EthanHolen <ethanholen@gmail.com>
2021-03-20 20:49:48 -0600

    Created AddVehicleView

---
+++ Happy Car/Views/AddVehicleView.swift
@@ -0,0 +1,49 @@
+//
+//  AddVehicleView.swift
+//  Happy Car
+//
+//  Created by Ethan Holen on 3/20/21.
+//
+
+import SwiftUI
+
+struct AddVehicleView: View {
+    @Environment(\.managedObjectContext) var moc
+    @Environment(\.presentationMode) var presentationMode
+    
+    @State private var name = ""
+    @State private var make = ""
+    @State private var model = ""
+    
+    var body: some View {
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Name (or nickname)", text: $name)
+                }
+                Section {
+                    TextField("Make of Vehicle", text: $make)
+                    TextField("Model of Vehicle", text: $model)
+                }
+            }
+            .navigationBarTitle("New Vehicle")
+            .navigationBarItems(trailing: Button("Save"){
+                let newVehicle = Vehicle(context: self.moc)
+                newVehicle.name = self.name
+                newVehicle.make = self.make
+                newVehicle.model = self.model
+                
+                if self.moc.hasChanges {
+                    try? self.moc.save()
+                }
+                self.presentationMode.wrappedValue.dismiss()
+            })
+        }
+    }
+}
+
+struct AddVehicleView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddVehicleView()
+    }
+}
