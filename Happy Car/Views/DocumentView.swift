1d1d36b0c5ba47ccf8be136e04483f41cb274302
EthanHolen <ethanholen@gmail.com>
2021-03-28 15:28:33 -0600

    Add basic document view

---
+++ Happy Car/Views/DocumentView.swift
@@ -0,0 +1,56 @@
+//
+//  DocumentView.swift
+//  Happy Car
+//
+//  Created by Ethan Holen on 3/27/21.
+//
+
+import SwiftUI
+import CoreData
+
+struct DocumentView: View {
+    
+    var document: Document
+    
+    var body: some View {
+        
+        
+        NavigationView {
+            
+            VStack{
+                Text(document.expiration ?? Date(), style: .date)
+                Text(document.wrappedNote)
+            }
+            
+            .navigationBarTitle(document.wrappedType)
+        }
+        
+        
+        
+        
+    }
+}
+
+struct DocumentView_Previews: PreviewProvider {
+    
+    
+    
+    
+    
+    //        static let moc = NSManagedObjectContext(concurrencyType: .mainQueueConcurrencyType)
+    
+    static var previews: some View {
+        
+        let moc = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
+        let sampleDocument = Document(context: moc)
+        sampleDocument.type = "Sample Type"
+        sampleDocument.expiration  = Date()
+        sampleDocument.note = "Sample Note"
+        
+        return DocumentView(document: sampleDocument)
+        
+        
+        
+    }
+}
